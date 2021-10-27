141f0604b243a8d230b3468378d64a4421a6db23
Michael Gillett <michaelgillett19@gmail.com>
2021-10-27 02:25:42 -0700

    added cool statuses

--- events/ready.js
+++ events/ready.js
@@ -6,9 +6,30 @@ const statuses = [
 	'Black Ops II',
 	'Realm Royale',
 	'Metroid™ Dread',
+	'PGA TOUR 2K21',
+	'Borderlands 3',
+	'Peggle 2',
+	'Back 4 Butt',
+	'Tannenberg',
+	'EDF 5',
+	'Gal Gun 2',
+	'Worms Battlegrounds',
+	'White Boyz Wit Attitude: The Pur$uit of Money',
+	'Destiny 3',
+	'GTA IV',
+	'Bloodborne',
+	'Otomedius X',
+	'Hello Kitty Online',
+	'Barbie Horse Adventures',
+	'Nancy Drew: Secret of the Scarlet Hand',
+	'AMBER: Journeys Beyond',
+	'The Crystal Key',
+	'NiBiRu: Age of Secrets',
+	'Freddi Fish 4: The Case of the Hogfish Rustlers of Briny Gulch',
 ];
 
 const { random } = require('../util/random');
+const { randomShuffle } = require('../util/shuffle');
 
 module.exports = {
 	name: 'ready',
@@ -18,18 +39,21 @@ module.exports = {
 
 		let counter = random(0, statuses.length);
 
+		let shuffledStatuses = randomShuffle(statuses);
+
 		const updateStatus = () => {
 			client.user?.setPresence({
 				status: 'online',
 				activities: [
 					{
-						name: statuses[counter],
+						name: shuffledStatuses[counter],
 					},
 				],
 			});
 
 			if (++counter >= statuses.length) {
 				counter = 0;
+				shuffledStatuses = randomShuffle(statuses);
 			}
 
 			setTimeout(updateStatus, 1000 * 60 * 15);
