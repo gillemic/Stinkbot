24986a9f0355633fec3c10450118858b5fbb2db0
Michael Gillett <michaelgillett19@gmail.com>
2021-12-31 19:00:45 -0800

    dubs function

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -17,6 +17,39 @@ module.exports = {
 			message.channel.send({ files: ['./img/OTAB.mp4'] });
 		}
 
+		if (doesContain(message.content, '-checkem')) {
+			const message_id = message.id;
+			const id_as_num = parseInt(message.id);
+
+			const last_digit = id_as_num % 10;
+
+			let count = 1;
+
+			while (last_digit === message_id[message_id.length - count]) {
+				count += 1;
+			}
+
+			if (count === 1) {
+				message.reply('Better luck next time, idiot');
+			}
+			else {
+				switch (count) {
+				case 2:
+					message.reply(`MessageID: ${message_id} Holy shit! You got dubs`);
+					break;
+				case 3:
+					message.reply(`MessageID: ${message_id} Holy shit! You got trips`);
+					break;
+				case 4:
+					message.reply(`MessageID: ${message_id} Holy shit! You got quads`);
+					break;
+				case 5:
+					message.reply(`MessageID: ${message_id} Holy shit! You got quints`);
+					break;
+				}
+			}
+		}
+
 		if (message.channel.id === '358699161551634442') {
 			// stink chat
 			return;
