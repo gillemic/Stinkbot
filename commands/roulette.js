6a3fa7243f58821d5de3fdd5eff60d3eaf12931c
Michael Gillett <michaelgillett19@gmail.com>
2022-02-03 20:33:09 -0800

    roulette fix

--- commands/roulette.js
+++ commands/roulette.js
@@ -7,28 +7,24 @@ module.exports = {
 		.setName('roulette')
 		.setDescription('Pick a user and play roulette. Don\'t lose though!')
 		.addUserOption(option =>
-			option.setName('user')
-				.setDescription('the member to try and mute')
+			option.setName('target')
+				.setDescription('the member to try and timeout')
 				.setRequired(true),
 		),
 	execute(interaction) {
 		const result = random(0, 5);
 
-		const victim = interaction.options.getMember('user');
+		const victim = interaction.options.getMember('target');
+
+		if (!interaction.guild.me.permissions.has(Permissions.FLAGS.MODERATE_MEMBERS)) {
+			interaction.reply({ content: 'Stinkbot does not have permissions to timeout members :(', ephemeral: true });
+			return;
+		}
 
 		if (interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR) || victim.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
 			interaction.reply({ content: 'One or more user(s) is an Administator of this guild and cannot be put in timeout', ephemeral: true });
 			return;
 		}
-
-		/* if (result != 2) {
-			interaction.member.timeout(1000 * 60 * 15, 'Owned idiot');
-			interaction.reply({ content: 'You lost! You\'ve been put in timeout for 15 minutes', ephemeral: true });
-		}
-		else {
-			user.member.timeout(1000 * 60 * 15, `You have been muted by ${interaction.member.displayName}`);
-			interaction.reply({ content: `You won! ${user.displayName} has been put in timeout for 15 minutes`, ephemeral: true });
-		} */
 
 		if (victim.moderatable) {
 			if (result != 2) {
