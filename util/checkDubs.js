ee2e2c63103e036ce6d18427abb7fd1a99785a86
Michael Gillett <michaelgillett19@gmail.com>
2022-02-22 16:57:15 -0800

    dubs changes

--- util/checkDubs.js
+++ util/checkDubs.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-const { containsAny } = require('./doesContain');
+const { doesContain, containsAny } = require('./doesContain');
 const { Permissions } = require('discord.js');
 
 const con = mysql.createConnection({
@@ -14,7 +14,7 @@ module.exports = {
 	checkDubs(message) {
 		const message_id = message.id;
 		const m = message.content.toLowerCase();
-		const words = ['dubs', 'dub5', 'doobs', 'd00bs', 'd00b5', 'doubles', 'doobles'];
+		const dubs_words = ['dubs', 'dub5', 'doobs', 'd00bs', 'd00b5', 'doubles', 'doobles'];
 
 		const last_digit = parseInt(message_id[message_id.length - 1]);
 
@@ -30,7 +30,7 @@ module.exports = {
 			switch (count) {
 			case 2:
 				column = 'dubs';
-				if (containsAny(m, words)) {
+				if (containsAny(m, dubs_words)) {
 					message.reply({ content: `MessageID: ${message_id} Holy shit! You got dubs`, files: ['./img/dubs.jpg'] });
 				}
 				break;
@@ -93,13 +93,38 @@ module.exports = {
 		}
 		else {
 			console.log('shut up >:(');
-			if (containsAny(m, words)) {
+			if (containsAny(m, dubs_words)) {
 				if (message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
 					message.reply({ content: 'You are an Administator of this guild and cannot be put in timeout' });
 					return;
 				}
 				message.member.timeout(1000 * 60 * 5, 'Owned idiot');
 				message.reply({ content: 'No dubs. You\'ve been put in timeout for 5 minutes.', files: ['./img/clown.jpg'] });
+			}
+			else if (doesContain(m, 'trips')) {
+				if (message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+					message.reply({ content: 'You are an Administator of this guild and cannot be put in timeout' });
+					return;
+				}
+				message.member.timeout(1000 * 60 * 10, 'Owned idiot');
+				message.reply({ content: 'No trips. You\'ve been put in timeout for 5 minutes.', files: ['./img/clown2.jpg'] });
+			}
+			else if (doesContain(m, 'quads')) {
+				if (message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+					message.reply({ content: 'You are an Administator of this guild and cannot be put in timeout' });
+					return;
+				}
+				message.member.timeout(1000 * 60 * 15, 'Owned idiot');
+				message.reply({ content: 'No quads. You\'ve been put in timeout for 15 minutes.', files: ['./img/clown3.jpg'] });
+			}
+			else if (doesContain(m, 'quints')) {
+				// soso
+				if (message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+					message.reply({ content: 'You are an Administator of this guild and cannot be put in timeout' });
+					return;
+				}
+				message.member.timeout(1000 * 60 * 30, 'Owned idiot');
+				message.reply({ content: 'No quints. You\'ve been put in timeout for 30 minutes.', files: ['./img/booboo.jpg'] });
 			}
 		}
 	},
