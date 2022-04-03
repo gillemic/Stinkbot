5e321a9d1f536f54fe1f752611d6183d4f1e8fe7
Michael Gillett <michaelgillett19@gmail.com>
2022-04-03 12:40:24 -0700

    dubs

--- util/checkDubs.js
+++ util/checkDubs.js
@@ -94,36 +94,24 @@ module.exports = {
 			});
 		}
 		else {
+			if (message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR) && containsAny(m, all_words)) {
+				message.react('❌');
+				return;
+			}
+
 			if (containsAny(m, dubs_words)) {
-				if (message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
-					message.react('❌');
-					return;
-				}
 				message.member.timeout(1000 * 60 * 5, 'Owned idiot');
 				message.reply({ content: 'No dubs. You\'ve been put in timeout for 5 minutes.', files: ['./img/clown.jpg'] });
 			}
 			else if (doesContain(m, 'trips')) {
-				if (message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
-					message.react('❌');
-					return;
-				}
 				message.member.timeout(1000 * 60 * 10, 'Owned idiot');
 				message.reply({ content: 'No trips. You\'ve been put in timeout for 10 minutes.', files: ['./img/clown2.jpg'] });
 			}
 			else if (doesContain(m, 'quads')) {
-				if (message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
-					message.react('❌');
-					return;
-				}
 				message.member.timeout(1000 * 60 * 15, 'Owned idiot');
 				message.reply({ content: 'No quads. You\'ve been put in timeout for 15 minutes.', files: ['./img/clown3.jpg'] });
 			}
 			else if (doesContain(m, 'quints')) {
-				// soso
-				if (message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
-					message.react('❌');
-					return;
-				}
 				message.member.timeout(1000 * 60 * 30, 'Owned idiot');
 				message.reply({ content: 'No quints. You\'ve been put in timeout for 30 minutes.', files: ['./img/booboo.jpg'] });
 			}
@@ -136,14 +124,11 @@ module.exports = {
 			return user.user.id;
 		});
 
-		console.log(userArray);
-
 		console.log('Connected. Displaying dubs leaderboard');
 		const sql = 'SELECT * FROM dubs ORDER BY quints DESC, quads DESC, trips DESC, dubs DESC';
 
 		con.query(sql, userArray, async (err, result) => {
 			if (err) throw err;
-			console.log(result);
 			let leaderboard = 'DUBS (CHECK EM) LEADERBOARD';
 			for (const i in result) {
 				if (!userArray.includes(result[i].UserID)) {
