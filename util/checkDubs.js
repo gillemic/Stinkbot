6746ce1d982f4d458f0b1931e35c0da95f96a321
Michael Gillett <michaelgillett19@gmail.com>
2022-05-30 21:26:23 -0700

    at everyone

--- util/checkDubs.js
+++ util/checkDubs.js
@@ -68,10 +68,10 @@ module.exports = {
 				if (doesContain(message.content, 'quints')) {
 					correct_call = true;
 				}
-				message.reply({ content: `MessageID: ${message_id} THATS QUINTS BABY!!`, files: ['./img/thefirstquints.png', './img/quints.png'] });
+				message.reply({ content: `@everyone MessageID: ${message_id} THATS QUINTS BABY!!`, files: ['./img/thefirstquints.png', './img/quints.png'] });
 				break;
 			default:
-				message.reply({ content: `MessageID: ${message_id} WOOOOOAAAAAHHHHH!!!!`, files: ['./img/holy.jpg'] });
+				message.reply({ content: `@everyone MessageID: ${message_id} WOOOOOAAAAAHHHHH!!!!`, files: ['./img/holy.jpg'] });
 				break;
 			}
 
