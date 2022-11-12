4e6e4955568ca6120fe3a4d0f5349b2b3ea98ece
Michael Gillett <michaelgillett19@gmail.com>
2022-11-12 15:22:27 -0800

    Stinkbox

--- commands/blend.js
+++ commands/blend.js
@@ -1,6 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { countImages } = require('../util/createBlend.js');
 const { loadAndProcessMyLocalImage } = require('../util/generateImage');
+const { containsAny } = require('../util/doesContain');
+
+const banned_words = ['blackface', 'black face', 'darkface', 'dark face', 'caricature', 'racist', 'racism', 'shaughn', 'shaaughn', 'squigger', 'squigga'];
+const banned_users = ['105884992055349248', '415407957371781123'];
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -14,6 +18,24 @@ module.exports = {
 		await interaction.deferReply();
 
 		const prompt = interaction.options.getString('prompt');
+
+		// Check if query is too long
+		if (prompt.length > 100) {
+			await interaction.editReply({ content: 'DALL·E: Invalid query\nQuery is too long.', ephemeral: true });
+			return;
+		}
+
+		// check if conor or justin between banned hours
+		/* if (interaction.user.id in banned_users):
+			await interaction.response.send_message("You are banned from blending fr", ephemeral=True)
+			return*/
+
+		// check if banned words
+		const query = prompt.toLowerCase();
+		if (containsAny(query, banned_words) && banned_users.includes(interaction.user.id)) {
+			await interaction.editReply({ content: 'Nope.', ephemeral: true });
+			return;
+		}
 
 		const folder = await countImages(prompt);
 
